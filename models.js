80b082aff6a028a58a194c6be3e1e84633310266
BrendaNamuh <brenda.namuhoranye@mail.mcgill.ca>
2022-09-15 15:34:22 -0400

    lr updates

--- models.js
+++ models.js
@@ -285,7 +285,7 @@ export class LinearRegression extends Model{
   
   //console.log("This is y1 "+ y1);
   console.log("This is y2 "+ y2);
-  return [parseFloat((m1*1)+(m2*1)+b).toFixed(2);, y2];
+  return [parseFloat((m1*1)+(m2*1)+b).toFixed(2), y2];
     }
 
     
